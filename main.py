192648ddc34eb93b9b1cddb38166cfd06c871202
Nicholas Rasi <nicholas.rasi@gmail.com>
2020-03-12 09:49:14 +0100

    create if not exists

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import argparse
-
 from flask import Flask, request
 from flask_cors import CORS
 from tinydb import TinyDB, Query
@@ -11,46 +10,47 @@ CORS(app)
 logging.basicConfig(level=logging.DEBUG)
 db = TinyDB('db.json')
 
+
+def create_new(bin_id):
+    db.insert({'id': bin_id,
+               'values': [],
+               'updated': str(datetime.datetime.now())})
+
+
+def get_bin_data(data_bin):
+    if len(data_bin) != 0:
+        return {'updated': data_bin[0]['updated'],
+                'values': data_bin[0]['values']}
+    else:
+        return {'error': 'bin not valid'}, 400
+
+
+def add_data(bin_id, data_bin):
+    values = data_bin[0]['values']
+    values.append(request.get_json())
+    db.update({'values': values, 'updated': str(datetime.datetime.now())}, Query().id == bin_id)
+    return {'msg': 'bin updated'}
+
+
 @app.route('/all')
 def get_all():
     data_bins = db.all()
     return {'bins': data_bins}
 
 
-@app.route('/new', methods=['POST'])
-def create_new():
-    request_data = request.get_json()
-    data_bin = db.search(Query().id == request_data["id"])
-    if len(data_bin) != 0 or str(request_data["id"]) == "new":
-        return {'error': 'bin already created'}, 400
-    else:
-        db.insert({'id': str(request_data["id"]),
-                   'desc': str(request_data["desc"]),
-                   'values': [],
-                   'updated': str(datetime.datetime.now())})
-        return {'msg': 'bin created'}
-
-
 @app.route('/<bin_id>', methods=['GET', 'POST'])
 def read_write(bin_id):
+    bin_id = str(bin_id)
+    data_bin = db.search(Query().id == bin_id)
     if request.method == 'GET':
-        data_bin = db.search(Query().id == bin_id)
-        if len(data_bin) != 0:
-            return {'desc': data_bin[0]['updated'],
-                    'updated': data_bin[0]['updated'],
-                    'values': data_bin[0]['values']}
-        else:
-            return {'error': 'bin not valid'}, 400
+        # return bin data if exists
+        return get_bin_data(data_bin)
     elif request.method == 'POST':
-        data_bin = db.search(Query().id == bin_id)
-        if len(data_bin) != 0:
-            values = data_bin[0]['values']
-            values.append(request.get_json())
-            db.update({'values': values,
-                       'updated': str(datetime.datetime.now())}, Query().id == bin_id)
-            return {'msg': 'bin updated'}
-        else:
-            return {'error': 'bin not valid'}, 400
+        if len(data_bin) == 0:
+            # bin not present -> create bin -> add value
+            create_new(bin_id)
+            data_bin = db.search(Query().id == bin_id)
+        return add_data(bin_id, data_bin)
 
 
 if __name__ == "__main__":
