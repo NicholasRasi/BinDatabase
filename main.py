896afd25c7251819b6858de650cf07b9d9c0ed13
Nicholas Rasi <nicholas.rasi@gmail.com>
2020-04-13 20:36:10 +0200

    added host as param

--- main.py
+++ main.py
@@ -64,6 +64,7 @@ def read_write(bin_id):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--host', type=int, default="0.0.0.0")
     parser.add_argument('--port', type=int, default=8080)
     args = parser.parse_args()
-    app.run(host='0.0.0.0', port=args.port)
+    app.run(host=args.host, port=args.port)
